87cd35b79be10341d2732f8bca2b7486d4e0510b
Matt Gardner <wilburnforce@gmail.com>
2019-10-02 14:43:25 -0400

    Remove vestigial webpack code

--- src/main.ts
+++ src/main.ts
@@ -23,11 +23,6 @@ async function bootstrap() {
   }));
 
   await app.listen(process.env.PORT || 3000);
-
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
 }
 
 bootstrap();
