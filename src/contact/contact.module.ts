251bec2e033f56a5bce6a4cde7cd12ffc469f48d
Matt Gardner <wilburnforce@gmail.com>
2019-10-31 22:16:40 -0400

    Testing setup (#17)
    * begin setting up testing
    
    * Passing test suite
    
    * add circle ci

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -3,13 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '../config/config.module';
 import { ContactService } from './contact.service';
 import { Contact } from './contact.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Contact]),
     ConfigModule,
   ],
-  providers: [ContactService],
+  providers: [
+    ContactService,
+    {
+      // how you provide the injection token in a test instance
+      provide: getRepositoryToken(Contact),
+      // as a class value, Repository needs no generics
+      useClass: Repository,
+    },
+  ],
   exports: [ContactService],
 })
 export class ContactModule {}
--- src/project/tiles/tiles.service.spec.ts
+++ src/project/tiles/tiles.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TilesService } from './tiles.service';
+import { Project } from '../project.entity';
+
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 describe('TilesService', () => {
   let service: TilesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TilesService],
+      providers: [
+        TilesService,
+        {
+          // how you provide the injection token in a test instance
+          provide: getRepositoryToken(Project),
+          // as a class value, Repository needs no generics
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<TilesService>(TilesService);
