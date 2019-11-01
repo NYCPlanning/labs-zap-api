55aa505ebead01a7a728f968716560af71b634a0
Matt Gardner <wilburnforce@gmail.com>
2019-10-31 22:22:24 -0400

    Hotfix test environment issue

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -5,7 +5,7 @@ export class ConfigService {
   private readonly envConfig: { [key: string]: string } = {};
 
   constructor(filePath: string) {
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test') {
       this.envConfig = process.env;
     } else {
       this.envConfig = dotenv.parse(fs.readFileSync(filePath)) || {};
